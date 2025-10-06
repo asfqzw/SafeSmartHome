9a515d9ad50734911dc62cfe8a91bd36fb9b2b8f
asfqzw <adrianniel6@gmail.com>
2025-10-06 13:47:39 +0800

    Add files via upload

---
+++ ESP32_CAM_Send_Photo_to_Google_Drive.ino
@@ -0,0 +1,618 @@
+String house = "House_1";
+
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+#include "Base64.h"
+#include <Human_Detection_inferencing.h>
+#include "edge-impulse-sdk/dsp/image/image.hpp"
+#include "esp_camera.h"
+//======================================== 
+
+//======================================== CAMERA_MODEL_AI_THINKER GPIO.
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+//======================================== 
+
+// LED Flash PIN (GPIO 4)
+#define FLASH_LED_PIN 4
+
+#define EI_CAMERA_RAW_FRAME_BUFFER_COLS           320
+#define EI_CAMERA_RAW_FRAME_BUFFER_ROWS           240
+#define EI_CAMERA_FRAME_BYTE_SIZE                 3
+
+static camera_config_t camera_config = {
+    .pin_pwdn = PWDN_GPIO_NUM,
+    .pin_reset = RESET_GPIO_NUM,
+    .pin_xclk = XCLK_GPIO_NUM,
+    .pin_sscb_sda = SIOD_GPIO_NUM,
+    .pin_sscb_scl = SIOC_GPIO_NUM,
+
+    .pin_d7 = Y9_GPIO_NUM,
+    .pin_d6 = Y8_GPIO_NUM,
+    .pin_d5 = Y7_GPIO_NUM,
+    .pin_d4 = Y6_GPIO_NUM,
+    .pin_d3 = Y5_GPIO_NUM,
+    .pin_d2 = Y4_GPIO_NUM,
+    .pin_d1 = Y3_GPIO_NUM,
+    .pin_d0 = Y2_GPIO_NUM,
+    .pin_vsync = VSYNC_GPIO_NUM,
+    .pin_href = HREF_GPIO_NUM,
+    .pin_pclk = PCLK_GPIO_NUM,
+
+    //XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
+    .xclk_freq_hz = 20000000,
+    .ledc_timer = LEDC_TIMER_0,
+    .ledc_channel = LEDC_CHANNEL_0,
+
+    .pixel_format = PIXFORMAT_JPEG, //YUV422,GRAYSCALE,RGB565,JPEG
+    .frame_size = FRAMESIZE_QVGA,    //QQVGA-UXGA Do not use sizes above QVGA when not JPEG
+
+    .jpeg_quality = 12, //0-63 lower number means higher quality
+    .fb_count = 1,       //if more than one, i2s runs in continuous mode. Use only with JPEG
+    .fb_location = CAMERA_FB_IN_PSRAM,
+    .grab_mode = CAMERA_GRAB_WHEN_EMPTY,
+};
+
+/* Function definitions ------------------------------------------------------- */
+bool ei_camera_init(void);
+void ei_camera_deinit(void);
+bool ei_camera_capture(uint32_t img_width, uint32_t img_height, uint8_t *out_buf) ;
+
+static bool debug_nn = false; // Set this to true to see e.g. features generated from the raw signal
+static bool is_initialised = false;
+uint8_t *snapshot_buf; //points to the output of the capture
+
+//======================================== Enter your WiFi ssid and password.
+const char* ssid = "wifi.";
+const char* password = "dejesus1922";
+//======================================== 
+
+//======================================== Replace with your "Deployment ID" and Folder Name.
+String myDeploymentID = "AKfycbyDtCGUr2b_AQBy265J2h9wehb7qUntAWhhV1NvagHTX-VXPgqZyilDpFh0Z9e7Q_m7";
+String myMainFolderName = "ESP32-CAM";
+String mySubFolderName = house;
+//======================================== 
+
+//======================================== Variables for Timer/Millis.
+unsigned long previousMillis = 0; 
+const int Interval = 5000; //--> Capture and Send a photo every 20 seconds.
+//======================================== 
+
+// Variable to set capture photo with LED Flash.
+// Set to "false", then the Flash LED will not light up when capturing a photo.
+// Set to "true", then the Flash LED lights up when capturing a photo.
+bool LED_Flash_ON = false;
+
+// Initialize WiFiClientSecure.
+WiFiClientSecure client;
+
+//________________________________________________________________________________ Test_Con()
+// This subroutine is to test the connection to "script.google.com".
+void Test_Con() {
+  const char* host = "script.google.com";
+  while(1) {
+    Serial.println("-----------");
+    Serial.println("Connection Test...");
+    Serial.println("Connect to " + String(host));
+  
+    client.setInsecure();
+  
+    if (client.connect(host, 443)) {
+      Serial.println("Connection successful.");
+      Serial.println("-----------");
+      client.stop();
+      break;
+    } else {
+      Serial.println("Connected to " + String(host) + " failed.");
+      Serial.println("Wait a moment for reconnecting.");
+      Serial.println("-----------");
+      client.stop();
+    }
+  
+    delay(1000);
+  }
+}
+//________________________________________________________________________________ 
+
+//________________________________________________________________________________ SendCapturedPhotos()
+// Subroutine for capturing and sending photos to Google Drive.
+void SendCapturedPhotos() {
+  const char* host = "script.google.com";
+  Serial.println();
+  Serial.println("-----------");
+  Serial.println("Connect to " + String(host));
+  
+  client.setInsecure();
+
+  //---------------------------------------- The Flash LED blinks once to indicate connection start.
+  // digitalWrite(FLASH_LED_PIN, HIGH);
+  delay(100);
+  // digitalWrite(FLASH_LED_PIN, LOW);
+  delay(100);
+  //---------------------------------------- 
+
+  //---------------------------------------- The process of connecting, capturing and sending photos to Google Drive.
+  if (client.connect(host, 443)) {
+    Serial.println("Connection successful.");
+    
+    if (LED_Flash_ON == true) {
+      // digitalWrite(FLASH_LED_PIN, HIGH);
+      delay(100);
+    }
+
+    //.............................. Taking a photo.
+    Serial.println();
+    Serial.println("Taking a photo...");
+    
+    for (int i = 0; i <= 3; i++) {
+      camera_fb_t * fb = NULL;
+      fb = esp_camera_fb_get();
+       if(!fb) {
+          Serial.println("Camera capture failed");
+          Serial.println("Restarting the ESP32 CAM.");
+          delay(1000);
+          ESP.restart();
+          return;
+        } 
+      esp_camera_fb_return(fb);
+      delay(200);
+    }
+  
+    camera_fb_t * fb = NULL;
+    fb = esp_camera_fb_get();
+    if(!fb) {
+      Serial.println("Camera capture failed");
+      Serial.println("Restarting the ESP32 CAM.");
+      delay(1000);
+      ESP.restart();
+      return;
+    } 
+  
+    if (LED_Flash_ON == true) // digitalWrite(FLASH_LED_PIN, LOW);
+    
+    Serial.println("Taking a photo was successful.");
+    //.............................. 
+
+    //.............................. Sending image to Google Drive.
+    Serial.println();
+    Serial.println("Sending image to Google Drive.");
+    Serial.println("Size: " + String(fb->len) + "byte");
+    
+    String url = "/macros/s/" + myDeploymentID + "/exec?folder=" + myMainFolderName + "&subfolder=" + mySubFolderName;
+
+
+    client.println("POST " + url + " HTTP/1.1");
+    client.println("Host: " + String(host));
+    client.println("Transfer-Encoding: chunked");
+    client.println();
+
+    int fbLen = fb->len;
+    char *input = (char *)fb->buf;
+    int chunkSize = 3 * 1000; //--> must be multiple of 3.
+    int chunkBase64Size = base64_enc_len(chunkSize);
+    char output[chunkBase64Size + 1];
+
+    Serial.println();
+    int chunk = 0;
+    for (int i = 0; i < fbLen; i += chunkSize) {
+      int l = base64_encode(output, input, min(fbLen - i, chunkSize));
+      client.print(l, HEX);
+      client.print("\r\n");
+      client.print(output);
+      client.print("\r\n");
+      delay(100);
+      input += chunkSize;
+      Serial.print(".");
+      chunk++;
+      if (chunk % 50 == 0) {
+        Serial.println();
+      }
+    }
+    client.print("0\r\n");
+    client.print("\r\n");
+
+    esp_camera_fb_return(fb);
+    //.............................. 
+
+    //.............................. Waiting for response.
+    Serial.println("Waiting for response.");
+    long int StartTime = millis();
+    while (!client.available()) {
+      Serial.print(".");
+      delay(100);
+      if ((StartTime + 10 * 1000) < millis()) {
+        Serial.println();
+        Serial.println("No response.");
+        break;
+      }
+    }
+    Serial.println();
+    while (client.available()) {
+      Serial.print(char(client.read()));
+    }
+    //.............................. 
+
+    //.............................. Flash LED blinks once as an indicator of successfully sending photos to Google Drive.
+    // digitalWrite(FLASH_LED_PIN, HIGH);
+    delay(500);
+    // digitalWrite(FLASH_LED_PIN, LOW);
+    delay(500);
+    //.............................. 
+  }
+  else {
+    Serial.println("Connected to " + String(host) + " failed.");
+    
+    //.............................. Flash LED blinks twice as a failed connection indicator.
+    // digitalWrite(FLASH_LED_PIN, HIGH);
+    delay(500);
+    // digitalWrite(FLASH_LED_PIN, LOW);
+    delay(500);
+    // digitalWrite(FLASH_LED_PIN, HIGH);
+    delay(500);
+    // digitalWrite(FLASH_LED_PIN, LOW);
+    delay(500);
+    //.............................. 
+  }
+  //---------------------------------------- 
+
+  Serial.println("-----------");
+
+  client.stop();
+}
+//________________________________________________________________________________ 
+
+//________________________________________________________________________________ VOID SETUP()
+void setup() {
+  // put your setup code here, to run once:
+  
+  // Disable brownout detector.
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+  
+  Serial.begin(115200);
+  Serial.println();
+  delay(1000);
+
+  pinMode(FLASH_LED_PIN, OUTPUT);
+  
+  // Setting the ESP32 WiFi to station mode.
+  Serial.println();
+  Serial.println("Setting the ESP32 WiFi to station mode.");
+  WiFi.mode(WIFI_STA);
+
+  //---------------------------------------- The process of connecting ESP32 CAM with WiFi Hotspot / WiFi Router.
+  Serial.println();
+  Serial.print("Connecting to : ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  
+  // The process timeout of connecting ESP32 CAM with WiFi Hotspot / WiFi Router is 20 seconds.
+  // If within 20 seconds the ESP32 CAM has not been successfully connected to WiFi, the ESP32 CAM will restart.
+  // I made this condition because on my ESP32-CAM, there are times when it seems like it can't connect to WiFi, so it needs to be restarted to be able to connect to WiFi.
+  int connecting_process_timed_out = 20; //--> 20 = 20 seconds.
+  connecting_process_timed_out = connecting_process_timed_out * 2;
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    // digitalWrite(FLASH_LED_PIN, HIGH);
+    delay(250);
+    // digitalWrite(FLASH_LED_PIN, LOW);
+    delay(250);
+    if(connecting_process_timed_out > 0) connecting_process_timed_out--;
+    if(connecting_process_timed_out == 0) {
+      Serial.println();
+      Serial.print("Failed to connect to ");
+      Serial.println(ssid);
+      Serial.println("Restarting the ESP32 CAM.");
+      delay(1000);
+      ESP.restart();
+    }
+  }
+
+  // digitalWrite(FLASH_LED_PIN, LOW);
+  
+  Serial.println();
+  Serial.print("Successfully connected to ");
+  Serial.println(ssid);
+  //Serial.print("ESP32-CAM IP Address: ");
+  //Serial.println(WiFi.localIP());
+  //---------------------------------------- 
+
+  //---------------------------------------- Set the camera ESP32 CAM.
+  Serial.println();
+  Serial.println("Set the camera ESP32 CAM...");
+  
+  
+  // init with high specs to pre-allocate larger buffers
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;  //0-63 lower number means higher quality
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 8;  //0-63 lower number means higher quality
+    config.fb_count = 1;
+  }
+  
+  // camera init
+  if (!ei_camera_init()) {
+  Serial.println("Camera initialization failed!");
+  ESP.restart();
+}
+
+
+  sensor_t * s = esp_camera_sensor_get();
+
+  // Selectable camera resolution details :
+  // -UXGA   = 1600 x 1200 pixels
+  // -SXGA   = 1280 x 1024 pixels
+  // -XGA    = 1024 x 768  pixels
+  // -SVGA   = 800 x 600   pixels
+  // -VGA    = 640 x 480   pixels
+  // -CIF    = 352 x 288   pixels
+  // -QVGA   = 320 x 240   pixels
+  // -HQVGA  = 240 x 160   pixels
+  // -QQVGA  = 160 x 120   pixels
+  s->set_framesize(s, FRAMESIZE_SXGA);  //--> UXGA|SXGA|XGA|SVGA|VGA|CIF|QVGA|HQVGA|QQVGA
+
+  Serial.println("Setting the camera successfully.");
+  Serial.println();
+
+  delay(1000);
+
+  Test_Con();
+
+  ei_camera_init();
+
+  Serial.println();
+  Serial.println("ESP32-CAM captures and sends photos to the server every 20 seconds.");
+  Serial.println();
+  delay(2000);
+} 
+//________________________________________________________________________________ 
+
+//________________________________________________________________________________ VOID LOOP()
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  
+    // instead of wait_ms, we'll wait on the signal, this allows threads to cancel us...
+    if (ei_sleep(5) != EI_IMPULSE_OK) {
+        return;
+    }
+
+    snapshot_buf = (uint8_t*)malloc(EI_CAMERA_RAW_FRAME_BUFFER_COLS * EI_CAMERA_RAW_FRAME_BUFFER_ROWS * EI_CAMERA_FRAME_BYTE_SIZE);
+
+    // check if allocation was successful
+    if(snapshot_buf == nullptr) {
+        ei_printf("ERR: Failed to allocate snapshot buffer!\n");
+        return;
+    }
+
+    ei::signal_t signal;
+    signal.total_length = EI_CLASSIFIER_INPUT_WIDTH * EI_CLASSIFIER_INPUT_HEIGHT;
+    signal.get_data = &ei_camera_get_data;
+
+    if (ei_camera_capture((size_t)EI_CLASSIFIER_INPUT_WIDTH, (size_t)EI_CLASSIFIER_INPUT_HEIGHT, snapshot_buf) == false) {
+        ei_printf("Failed to capture image\r\n");
+        free(snapshot_buf);
+        return;
+    }
+
+    // Run the classifier
+    ei_impulse_result_t result = { 0 };
+
+    EI_IMPULSE_ERROR err = run_classifier(&signal, &result, debug_nn);
+    if (err != EI_IMPULSE_OK) {
+        ei_printf("ERR: Failed to run classifier (%d)\n", err);
+        return;
+    }
+
+    // print the predictions
+    ei_printf("Predictions (DSP: %d ms., Classification: %d ms., Anomaly: %d ms.): \n",
+                result.timing.dsp, result.timing.classification, result.timing.anomaly);
+
+#if EI_CLASSIFIER_OBJECT_DETECTION == 1
+    ei_printf("Object detection bounding boxes:\r\n");
+    for (uint32_t i = 0; i < result.bounding_boxes_count; i++) {
+        ei_impulse_result_bounding_box_t bb = result.bounding_boxes[i];
+        if (bb.value == 0) {
+            continue;
+        }
+        if(bb.value > .7){
+          SendCapturedPhotos();
+        }
+        ei_printf("  %s (%f) [ x: %u, y: %u, width: %u, height: %u ]\r\n",
+                bb.label,
+                bb.value,
+                bb.x,
+                bb.y,
+                bb.width,
+                bb.height);
+    }
+
+    // Print the prediction results (classification)
+#else
+    ei_printf("Predictions:\r\n");
+    for (uint16_t i = 0; i < EI_CLASSIFIER_LABEL_COUNT; i++) {
+        ei_printf("  %s: ", ei_classifier_inferencing_categories[i]);
+        ei_printf("%.5f\r\n", result.classification[i].value);
+    }
+#endif
+
+    // Print anomaly result (if it exists)
+#if EI_CLASSIFIER_HAS_ANOMALY
+    ei_printf("Anomaly prediction: %.3f\r\n", result.anomaly);
+#endif
+
+#if EI_CLASSIFIER_HAS_VISUAL_ANOMALY
+    ei_printf("Visual anomalies:\r\n");
+    for (uint32_t i = 0; i < result.visual_ad_count; i++) {
+        ei_impulse_result_bounding_box_t bb = result.visual_ad_grid_cells[i];
+        if (bb.value == 0) {
+            continue;
+        }
+        ei_printf("  %s (%f) [ x: %u, y: %u, width: %u, height: %u ]\r\n",
+                bb.label,
+                bb.value,
+                bb.x,
+                bb.y,
+                bb.width,
+                bb.height);
+    }
+#endif
+
+
+    free(snapshot_buf);
+
+}
+
+bool ei_camera_init(void) {
+
+    if (is_initialised) return true;
+
+#if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+#endif
+
+    //initialize the camera
+    esp_err_t err = esp_camera_init(&camera_config);
+    if (err != ESP_OK) {
+      Serial.printf("Camera init failed with error 0x%x\n", err);
+      return false;
+    }
+
+    sensor_t * s = esp_camera_sensor_get();
+    // initial sensors are flipped vertically and colors are a bit saturated
+    if (s->id.PID == OV3660_PID) {
+      s->set_vflip(s, 1); // flip it back
+      s->set_brightness(s, 1); // up the brightness just a bit
+      s->set_saturation(s, 0); // lower the saturation
+    }
+
+#if defined(CAMERA_MODEL_M5STACK_WIDE)
+    s->set_vflip(s, 1);
+    s->set_hmirror(s, 1);
+#elif defined(CAMERA_MODEL_ESP_EYE)
+    s->set_vflip(s, 1);
+    s->set_hmirror(s, 1);
+    s->set_awb_gain(s, 1);
+#endif
+
+    is_initialised = true;
+    return true;
+}
+
+/**
+ * @brief      Stop streaming of sensor data
+ */
+void ei_camera_deinit(void) {
+
+    //deinitialize the camera
+    esp_err_t err = esp_camera_deinit();
+
+    if (err != ESP_OK)
+    {
+        ei_printf("Camera deinit failed\n");
+        return;
+    }
+
+    is_initialised = false;
+    return;
+}
+
+
+/**
+ * @brief      Capture, rescale and crop image
+ *
+ * @param[in]  img_width     width of output image
+ * @param[in]  img_height    height of output image
+ * @param[in]  out_buf       pointer to store output image, NULL may be used
+ *                           if ei_camera_frame_buffer is to be used for capture and resize/cropping.
+ *
+ * @retval     false if not initialised, image captured, rescaled or cropped failed
+ *
+ */
+bool ei_camera_capture(uint32_t img_width, uint32_t img_height, uint8_t *out_buf) {
+    bool do_resize = false;
+
+    if (!is_initialised) {
+        ei_printf("ERR: Camera is not initialized\r\n");
+        return false;
+    }
+
+    camera_fb_t *fb = esp_camera_fb_get();
+
+    if (!fb) {
+        ei_printf("Camera capture failed\n");
+        return false;
+    }
+
+   bool converted = fmt2rgb888(fb->buf, fb->len, PIXFORMAT_JPEG, snapshot_buf);
+
+   esp_camera_fb_return(fb);
+
+   if(!converted){
+       ei_printf("Conversion failed\n");
+       return false;
+   }
+
+    if ((img_width != EI_CAMERA_RAW_FRAME_BUFFER_COLS)
+        || (img_height != EI_CAMERA_RAW_FRAME_BUFFER_ROWS)) {
+        do_resize = true;
+    }
+
+    if (do_resize) {
+        ei::image::processing::crop_and_interpolate_rgb888(
+        out_buf,
+        EI_CAMERA_RAW_FRAME_BUFFER_COLS,
+        EI_CAMERA_RAW_FRAME_BUFFER_ROWS,
+        out_buf,
+        img_width,
+        img_height);
+    }
+
+
+    return true;
+}
+
+static int ei_camera_get_data(size_t offset, size_t length, float *out_ptr)
+{
+    // we already have a RGB888 buffer, so recalculate offset into pixel index
+    size_t pixel_ix = offset * 3;
+    size_t pixels_left = length;
+    size_t out_ptr_ix = 0;
+
+    while (pixels_left != 0) {
+        // Swap BGR to RGB here
+        // due to https://github.com/espressif/esp32-camera/issues/379
+        out_ptr[out_ptr_ix] = (snapshot_buf[pixel_ix + 2] << 16) + (snapshot_buf[pixel_ix + 1] << 8) + snapshot_buf[pixel_ix];
+
+        // go to the next pixel
+        out_ptr_ix++;
+        pixel_ix+=3;
+        pixels_left--;
+    }
+    // and done!
+    return 0;
+}
+
+#if !defined(EI_CLASSIFIER_SENSOR) || EI_CLASSIFIER_SENSOR != EI_CLASSIFIER_SENSOR_CAMERA
+#error "Invalid model for current sensor"
+#endif
